35f828c8ebd224f89f317476ec8a22ffddef5bf5
Piotr Grzela <piotr.grzela@op.pl>
2023-02-19 03:43:56 +0100

    test

--- src/main/java/pl/coderslab/afirstprogram/Main02.java
+++ src/main/java/pl/coderslab/afirstprogram/Main02.java
@@ -2,7 +2,7 @@ package pl.coderslab.afirstprogram;
 
 public class Main02 {
     public static void main(String[] args) {
-        System.out.println("Hello Java1");
-        System.out.println("Hello Coderslab1");
+        System.out.println("Hello Java");
+        System.out.println("Hello Coderslab");
     }
 }
