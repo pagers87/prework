8143738380e5cc99629f916a338fbc5e0954fda8
Piotr Grzela <125509962+pagers87@users.noreply.github.com>
2023-02-20 19:40:57 +0100

    Update Main02.java

--- src/main/java/pl/coderslab/afirstprogram/Main02.java
+++ src/main/java/pl/coderslab/afirstprogram/Main02.java
@@ -2,14 +2,7 @@ package pl.coderslab.afirstprogram;
 
 public class Main02 {
     public static void main(String[] args) {
-        /*
-        String result = "Hello Java" + System.lineSeparator() + "Hello Coderslab";
-        System.out.println(result);
-         */
-
-
         System.out.println("Hello Java");
-        System.out.println("Hello Coderslab");
-
+        System.out.println("Hello Coderslab11");
     }
 }
